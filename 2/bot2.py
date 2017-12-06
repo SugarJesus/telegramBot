775665f2843ebb326540b0dd398d4609acf827c0
SugarJesus <gugnt777@mail.ru>
2017-12-06 20:31:41 +0300

    закоментил

--- 2/bot2.py
+++ 2/bot2.py
@@ -1,27 +1,28 @@
-import telebot
-from telebot import types
+#для начала нужно установить библиотеку pyTelegramBotAPI
 
-token = 'TOKEN'
+import telebot
+from telebot import types    #для подключения клавиатуры
+
+token = 'TOKEN' #токен бота выданный Botfather
 bot = telebot.TeleBot(token)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start'])  #запускаем хэндлер (специальный обработчик для разных типов сообщений) обрабатывающий команду "старт"
 def start(message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)  #создаем клавиатуру и меняем размер, чтобы кнопки не были огромными
     keyboard.add(*[types.KeyboardButton(name) for name in ['Sayori',
-        'Natsuki', 'Yuri', 'Monika']])
-    msg = bot.send_message(message.chat.id, 'Who will you choose?',
-        reply_markup=keyboard)
-    bot.register_next_step_handler(msg, girl)
+        'Natsuki', 'Yuri', 'Monika']])  #небольшой цикл, чтобы не задавать каждую кнопку отдельно
+    bot.send_message(message.chat.id, 'Who will you choose?',   #текстовый ответ бота на команду старт
+        reply_markup=keyboard) #завершаем создание клавиатуры
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text'])   #хэндлер обрабатывающий наши текстовые сообщения
 def girl(message):
-    if message.text == "Sayori":
+    if message.text == "Sayori":   #запускаем оператор ветвления реагирующий на определенные слова
         bot.send_message(message.chat.id,
-        'ehehe...')
-        bot.send_photo(message.chat.id,
-        photo='https://tcrf.net/images/thumb/5/5f/DDLC-Sayuri-early.png/320px-DDLC-Sayuri-early.png')
-    elif message.text == "Natsuki":
+        'ehehe...')              #текстовый ответ бота на определенное сообщение
+        bot.send_photo(message.chat.id,      #отправляем картинку на определенное сообщение
+        photo='https://tcrf.net/images/thumb/5/5f/DDLC-Sayuri-early.png/320px-DDLC-Sayuri-early.png')  #
+    elif message.text == "Natsuki":           #повторяем в зависимости от количества нужных нам вариантов у нас их всего 4
         bot.send_message(message.chat.id,
         'Baka!')
         bot.send_photo(message.chat.id,
@@ -36,10 +37,9 @@ def girl(message):
         'Just Monika')
         bot.send_photo(message.chat.id,
         photo='https://tcrf.net/images/thumb/1/12/DDLC-Monika-early.png/320px-DDLC-Monika-early.png')
-    else:
-        bot.send_message(
-        message.chat.id,
-        '{ololo} is not in the club!'.format(ololo=message.text))
-
+    else:    #закрываем оператор ветвления
+        bot.send_message(message.chat.id,
+        '{ololo} is not in the club!'.format(ololo=message.text))  #на любые сообщения не удовлетворяющие описанным выше бот будет отвечать
+                                                                   #текстом этого сообщения и припиской о том, что "текст" не состоит в клубе
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+     bot.polling(none_stop=True)  #запускаем лонг пулинг, параметр говорит, что бот должен стараться не прекращать работу при возникновении ошибок
--- 3/bot3.py
+++ 3/bot3.py
@@ -1,17 +1,17 @@
 import telebot
-import os
-import random
+import os   #подключаем модуль ос, нужен будет для получения списка файлов в папке
+import random  #модуль рандом... для рандома
 
-token = 'TOKEN'
+token = 'TOKEN' #токен
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(content_types=['text'])
-def send_rand_photo(message):
-    directory = 'C:/telegramBot/3/img/'
-    files = os.listdir(directory)
-    random_file = random.choice(files)
-    photo = open(directory + '/' + random_file, 'rb')
-    bot.send_photo(message.chat.id, photo)
+@bot.message_handler(content_types=['text']) #хэндлер обрабатывающий любой введенный нами текст
+def rand_photo(message):
+    directory = 'C:/telegramBot/3/img/'  #присваеваем переменной директори значение пути к папке с нашими фоточками
+    files = os.listdir(directory)  #присваеваем переменной файлс значение соответствующее списку всех имен файлов в нашей папке
+    random_file = random.choice(files)  #присваиваем переменной рэндомфайл значение равное случайно выбранному имени файла из нашего списка
+    photo = open(directory + '/' + random_file, 'rb')  #присваиваем переменной фото значение пути к нашему случайному файлику
+    bot.send_photo(message.chat.id, photo) #бот посылает нам случайное фото
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+     bot.polling(none_stop=True)  #и опять пулим
